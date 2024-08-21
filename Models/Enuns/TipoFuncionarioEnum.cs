fb038d53847076051589613244bb7eb481ffab04
Vinicius Gomes <vitrigom5@gmail.comS>
2024-08-21 19:56:20 -0300

    Exercicios

---
+++ Models/Enuns/TipoFuncionarioEnum.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aula03Colecoes
+{
+    public enum TipoFuncionarioEnum
+    {
+        CLT = 1,
+        Aprendiz = 2
+    }
+}
